5c5ee9b5f21eeb1d835c91c2aab9cd3b2763a494
Ray Hancock <ray.hancock@aprimo.com>
2020-07-25 20:40:37 -0400

    added tutorial14

---
+++ Tutorial014/Component.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tutorial014
+{
+	public abstract class Component
+	{
+		public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+		public abstract void Update(GameTime gameTime);
+	}
+}
